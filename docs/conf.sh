9630dcbc6ebf0f5d93053ba1823cd9e6ea7fc921
Liza <vorrrl@mail.ru>
2021-03-08 13:12:36 +0300

    Fixed file conf.sh on branch1

--- docs/conf.sh
+++ docs/conf.sh
@@ -7,7 +7,7 @@
     #so feel free to get rid of anything you don't want in here
 
 ###################
-#  ENV VARIABLES  #
+#  Some the changes  #
 ###################
 
 #each variable below can be set here, or using export
